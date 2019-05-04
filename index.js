8aa4ee4807ad3b5839a7fcd78acdaf8b2f125dad
Omar Patel <omarpatel123@yahoo.com>
2019-05-04 10:37:13 -0700

    add basics of Spice Girls code

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Yo, tell me what you want, what you really really want");
+
+function SpiceGirls(WhatIWant) {
+    return WhatIWant;
+}
+
+var myLovah = SpiceGirls("Be my Lovah");
+
+console.log(myLovah);
