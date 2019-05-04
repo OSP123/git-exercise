91df74d2f7be0ff00d4e6aa245acbf33a8323683
milkncookiesss <p.parkcontact@gmail.com>
2019-05-04 10:38:14 -0700

    "create test to expect 46k > 45k"

---
+++ wh46k.js
@@ -0,0 +1,9 @@
+
+
+describe('War Hammer 46k', () => {
+  it('should be better than 45k', () => {
+    let fourtySix = 46000;
+    let fourtyFive = 45000;
+    expect(fourtySix).to.be.above(fourtyFive);
+  })
+})
