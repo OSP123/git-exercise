ee77da5ca8b537ec82c604a187dc7e16ea989928
Jeffrey Lee <jeffylee87@hotmail.com>
2019-05-04 10:39:34 -0700

    added anyongjs file

---
+++ anyong.js
@@ -0,0 +1,9 @@
+function anyong (count) {
+    for(var i = 0; i < count; i++){
+        console.log("Anyong");
+        if(i > 2){
+            console.log("ANYONG!!!!!!");
+        }
+    }
+}
+anyong(1000000);
